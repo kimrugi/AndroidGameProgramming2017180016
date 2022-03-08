3ec00bffcaf9a8570bf348743c1083200a046be0
kimrugi <kimrugi@gmail.com>
2022-03-08 15:01:27 +0900

    Create Empty Android Project

---
+++ app/src/main/java/com/tu/agp/androidgametermproject/androidgameprogramming_2017180016/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tu.agp.androidgametermproject.androidgameprogramming_2017180016;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
