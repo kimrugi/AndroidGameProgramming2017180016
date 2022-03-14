f25c6cc5bbb0fa7a180bbeee8820cc7f29e74098
kimrugi <kimrugi@gmail.com>
2022-03-14 17:16:23 +0900

    local variable -> field
    Rename tv -> subTextView

--- app/src/main/java/com/tu/agp/androidgametermproject/androidgameprogramming_2017180016/MainActivity.java
+++ app/src/main/java/com/tu/agp/androidgametermproject/androidgameprogramming_2017180016/MainActivity.java
@@ -4,26 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.QuickContactBadge;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView subTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.subText);
-        tv.setText("i am a good programmer");
+        subTextView = findViewById(R.id.subText);
+        subTextView.setText("i am a good programmer");
 
 
     }
 
 
     public void onBtnPushMe(View view) {
-        TextView tv = findViewById(R.id.subText);
-        tv.setText("Clicked \\(OoO)/");
+//        TextView tv = findViewById(R.id.subText);
+        subTextView.setText("Clicked \\(OoO)/");
     }
 }
