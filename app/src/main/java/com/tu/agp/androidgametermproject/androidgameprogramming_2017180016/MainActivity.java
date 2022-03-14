9dda7257827ca7a123c6f44260aad5102ea9c324
kimrugi <kimrugi@gmail.com>
2022-03-14 16:59:42 +0900

    Add button
    findViewById, setText, setOnClickListened

--- app/src/main/java/com/tu/agp/androidgametermproject/androidgameprogramming_2017180016/MainActivity.java
+++ app/src/main/java/com/tu/agp/androidgametermproject/androidgameprogramming_2017180016/MainActivity.java
@@ -3,6 +3,10 @@ package com.tu.agp.androidgametermproject.androidgameprogramming_2017180016;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.subText);
+        tv.setText("i am a good programmer");
+
+        Button btn = findViewById(R.id.btnPushMe);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("Clicked \\(OoO)/");
+            }
+        });
     }
 
 }
