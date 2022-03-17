f0e66d81407c7e33405ed0a63a1a734fed7dfbb5
kimrugi <kimrugi@gmail.com>
2022-03-17 18:34:38 +0900

    Make empty project

--- Projects/7ImageSwitcher/app/src/main/java/com/tu/agp/androidgametermproject/androidgameprogramming_2017180016/MainActivity.java
+++ Projects/7ImageSwitcher/app/src/main/java/com/tu/agp/androidgametermproject/androidgameprogramming_2017180016/MainActivity.java
@@ -8,22 +8,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView subTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        subTextView = findViewById(R.id.subText);
-        subTextView.setText("i am a good programmer");
-
-
     }
 
-
-    public void onBtnPushMe(View view) {
-//        TextView tv = findViewById(R.id.subText);
-        subTextView.setText("Clicked \\(OoO)/");
-    }
 }
