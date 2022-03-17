59eb05abfbc044dba40280dd961ae5ad840db77b
kimrugi <kimrugi@gmail.com>
2022-03-17 20:15:36 +0900

    Add ids and onClick
    Add ids on TextView, ImageView
    Add onClick on Buttons

--- Projects/7ImageSwitcher/app/src/main/java/com/tu/agp/androidgametermproject/androidgameprogramming_2017180016/MainActivity.java
+++ Projects/7ImageSwitcher/app/src/main/java/com/tu/agp/androidgametermproject/androidgameprogramming_2017180016/MainActivity.java
@@ -4,13 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView pageNumberText;
+    private int pageNumber;
+    private ImageView catImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        catImage = findViewById(R.id.catImage);
+        pageNumberText = findViewById(R.id.pageNumber);
+        pageNumber = 1;
+
     }
+
+    public void prevBtn(View view) {
+
+    }
+
+    public void nextBtn(View view) {
+
+    }
+
 
 }
