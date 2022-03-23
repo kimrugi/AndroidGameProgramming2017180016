a68a473a5399dda9093415ca2b3f44aa27a0b559
kimrugi <kimrugi@gmail.com>
2022-03-23 12:46:14 +0900

    card app 완성

--- Projects/Card/app/src/main/java/kr/ac/tukorea/k2017180016/card/MainActivity.java
+++ Projects/Card/app/src/main/java/kr/ac/tukorea/k2017180016/card/MainActivity.java
@@ -4,23 +4,27 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private static final String tag = MainActivity.class.getSimpleName();
     private static final int[] btnIds = new int[]{
-            R.id.card01,R.id.card01,R.id.card02,R.id.card03,
+            R.id.card00,R.id.card01,R.id.card02,R.id.card03,
             R.id.card10,R.id.card11,R.id.card12,R.id.card13,
             R.id.card20,R.id.card21,R.id.card22,R.id.card23,
             R.id.card30,R.id.card31,R.id.card32,R.id.card33
     };
-    private int[] cardimage = new int[]{
+    private int[] resIds = new int[]{
             R.mipmap.card_as,R.mipmap.card_2c,R.mipmap.card_3d,R.mipmap.card_4h,
             R.mipmap.card_5s,R.mipmap.card_jc,R.mipmap.card_kd,R.mipmap.card_qh,
             R.mipmap.card_as,R.mipmap.card_2c,R.mipmap.card_3d,R.mipmap.card_4h,
@@ -28,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private ImageButton previousButton;
-    private int flips = 0;
+    private int flips = 16;
+    private int openCardCount;
     private TextView scoreTextView;
 
     @Override
@@ -41,11 +46,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startGame() {
-        for(int i = 0; i < btnIds.length; ++i) {
-            int resid = cardimage[i];
-            ImageButton btn = findViewById(btnIds[i]);
-            btn.setTag(resid);
+        Random random = new Random();
+        for(int i = 0; i < resIds.length; ++i){
+            int t = random.nextInt(resIds.length);
+            int id = resIds[i];
+            resIds[i] = resIds[t];
+            resIds[t] = id;
         }
+        for(int i = 0; i < btnIds.length; ++i) {
+            int resid = resIds[i];
+            ImageButton btn = findViewById(btnIds[i]);
+            btn.setVisibility(View.VISIBLE);
+            btn.setTag(resid);
+            //btn.setImageResource(R.mipmap.card_blue_back);
+            btn.setImageResource(resIds[i]);
+        }
+        openCardCount = resIds.length;
+        setScore(0);
+        previousButton = null;
     }
 
     public void onBtnRestart(View view) {
@@ -56,15 +74,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void askRetry() {
         new AlertDialog.Builder(this)
-                .setTitle("Restart")
-                .setMessage("Really restart game?")
-                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                .setTitle(R.string.restart)
+                .setMessage(R.string.restart_alert_message)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         startGame();
                     }
                 })
-                .setNegativeButton("no", null)
+                .setNegativeButton(R.string.no, null)
                 .create().show();
     }
 
@@ -77,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
         if(previousButton != null) {
             if (imageButton == previousButton) {
                 Log.d(tag, "Same Button Pressed");
+                Toast.makeText(this, R.string.same_button_pressed, Toast.LENGTH_SHORT).show();
                 return;
             }
             Log.d(tag, "Pressed Card" + imageButton.getId());
@@ -89,10 +108,14 @@ public class MainActivity extends AppCompatActivity {
             if (resid != prevResid) {
                 previousButton.setImageResource(R.mipmap.card_blue_back);
             } else {
-                setScore(flips);
+                setScore(flips+1);
+                openCardCount = openCardCount - 2;
                 imageButton.setVisibility(View.INVISIBLE);
                 previousButton.setVisibility(View.INVISIBLE);
                 previousButton = null;
+                if (openCardCount <= 0){
+                    askRetry();
+                }
             }
         }
         previousButton = imageButton;
@@ -101,7 +124,10 @@ public class MainActivity extends AppCompatActivity {
     private void setScore(int flips) {
         this.flips = flips;
 
-        scoreTextView.setText("Score: " + flips);
+        Resources res = getResources();
+        String fmt = res.getString(R.string.flips_fmt);
+        String text = String.format(fmt, flips);
+        scoreTextView.setText(text);
     }
 
     private int findBtnIndex(int id) {
