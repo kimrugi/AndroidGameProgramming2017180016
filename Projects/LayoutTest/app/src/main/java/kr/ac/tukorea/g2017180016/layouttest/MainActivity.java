0563749d48a3cc8977622fa541093ec2e460a348
kimrugi <kimrugi@gmail.com>
2022-03-23 12:46:25 +0900

    Create LayoutTest

---
+++ Projects/LayoutTest/app/src/main/java/kr/ac/tukorea/g2017180016/layouttest/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.g2017180016.layouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
