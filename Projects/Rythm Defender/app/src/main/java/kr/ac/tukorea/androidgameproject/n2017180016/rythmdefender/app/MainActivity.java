ccc8dfd091b1a4b0128d376d4587338190aabf7d
kimrugi <kimrugi@gmail.com>
2022-04-27 22:32:03 +0900

    Change Setting to Fullscreen mode

--- Projects/Rythm Defender/app/src/main/java/kr/ac/tukorea/androidgameproject/n2017180016/rythmdefender/app/MainActivity.java
+++ Projects/Rythm Defender/app/src/main/java/kr/ac/tukorea/androidgameproject/n2017180016/rythmdefender/app/MainActivity.java
@@ -3,6 +3,8 @@ package kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.R;
 
@@ -11,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
         setContentView(R.layout.activity_main);
+
     }
 }
