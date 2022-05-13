1c4274d8b6eca9f9f07c981394ddcffc2eebf8fa
kimrugi <kimrugi@gmail.com>
2022-05-13 20:13:43 +0900

    채보 재작용 activity 추가

---
+++ Projects/Rythm Defender/app/src/main/java/kr/ac/tukorea/androidgameproject/n2017180016/rythmdefender/app/ChartMakingActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.R;
+
+public class ChartMakingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_making);
+    }
+}
