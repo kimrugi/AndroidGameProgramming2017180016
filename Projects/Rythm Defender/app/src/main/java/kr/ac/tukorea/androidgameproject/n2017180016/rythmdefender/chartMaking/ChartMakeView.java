b153455f67ed2b2724d58ca38c29b03366338c34
kimrugi <kimrugi@gmail.com>
2022-05-19 19:29:23 +0900

    채보 제작 뷰 추가 & GameView가 MainGame을 가지도록 변경

---
+++ Projects/Rythm Defender/app/src/main/java/kr/ac/tukorea/androidgameproject/n2017180016/rythmdefender/chartMaking/ChartMakeView.java
@@ -0,0 +1,23 @@
+package kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.chartMaking;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.Choreographer;
+
+import androidx.annotation.Nullable;
+
+import kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.framework.GameView;
+import kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.game.MainGame;
+
+public class ChartMakeView extends GameView {
+    public ChartMakeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        MainGame
+    }
+}
