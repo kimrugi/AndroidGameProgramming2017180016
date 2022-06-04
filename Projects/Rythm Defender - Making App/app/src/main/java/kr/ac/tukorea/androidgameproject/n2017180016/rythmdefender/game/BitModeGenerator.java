53711e54065bdffe31fcd2303be1c2070fefd1fe
kimrugi <kimrugi@gmail.com>
2022-06-04 16:54:44 +0900

    bit를 기록하고 보여줄 class BitModeGenerator생성

---
+++ Projects/Rythm Defender - Making App/app/src/main/java/kr/ac/tukorea/androidgameproject/n2017180016/rythmdefender/game/BitModeGenerator.java
@@ -0,0 +1,20 @@
+package kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.game;
+
+public class BitModeGenerator extends ObjectGenerator{
+    public enum Position {
+        right, left
+    }
+
+    BitModeGenerator(String jsonFileName) {
+        super(jsonFileName);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    void addArrow(Position pos){
+
+    }
+}
