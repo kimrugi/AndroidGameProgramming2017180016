4af33470b5f38f2954a3ceace4661129f9769130
kimrugi <kimrugi@gmail.com>
2022-06-12 19:44:55 +0900

    MainActivity에 listview를 넣어서 선택하고 플레이하게 함

--- Projects/Rythm Defender/app/src/main/java/kr/ac/tukorea/androidgameproject/n2017180016/rythmdefender/app/GameActivity.java
+++ Projects/Rythm Defender/app/src/main/java/kr/ac/tukorea/androidgameproject/n2017180016/rythmdefender/app/GameActivity.java
@@ -2,6 +2,7 @@ package kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -10,7 +11,9 @@ import kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.R;
 import kr.ac.tukorea.androidgameproject.n2017180016.rythmdefender.game.MainGame;
 
 public class GameActivity extends AppCompatActivity {
-
+    public static final String MUSIC = "MUSIC_FILE_NAME";
+    public static final String CHART = "CHART_FILE_NAME";
+    public static final String IMAGE = "IMAGE_FILE_NAME";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +25,17 @@ public class GameActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
-        MainGame.getInstance().setMusic("lune_8bit.mp3");
-        MainGame.getInstance().setChart("Lune.json");
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras == null) {
+            finish();
+            return;
+        }
+        String musicFile = extras.getString(GameActivity.MUSIC);
+        String chartFile = extras.getString(GameActivity.CHART);
+        String imageFile = extras.getString(GameActivity.IMAGE);
+        MainGame.getInstance().setMusic(musicFile);
+        MainGame.getInstance().setChart(chartFile);
 
         setContentView(R.layout.activity_game);
     }
